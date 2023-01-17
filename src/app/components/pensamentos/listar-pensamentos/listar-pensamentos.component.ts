994fee415c74314a89217a12bbe8cdec5dd4ec41
Lucas Martins <lucasifbaeuna@gmail.com>
2023-01-17 15:57:02 -0300

    feat: aplicando as diretivas do angular

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
-  styleUrls: ['./listar-pensamentos.component.scss']
+  styleUrls: ['./listar-pensamentos.component.scss'],
 })
-export class ListarPensamentosComponent {
+export class ListarPensamentosComponent implements OnInit {
+  listPensamentos = [
+    {
+      conteudo: 'Passo informações para componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio dolores libero, quibusdam reprehenderit nemo in molestias atque quod totam est explicabo consequatur aliquid eos architecto necessitatibus praesentium maxime amet accusamus! zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzs',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/pensamentos/pensamento-card/pensamento-card.component.ts
+++ src/app/components/pensamentos/pensamento-card/pensamento-card.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento-card',
   templateUrl: './pensamento-card.component.html',
-  styleUrls: ['./pensamento-card.component.scss']
+  styleUrls: ['./pensamento-card.component.scss'],
 })
 export class PensamentoCardComponent implements OnInit {
+  @Input()
   pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Lucas',
+    modelo: 'modelo3',
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    } else {
+      return 'pensamento-p';
+    }
   }
-
-  constructor(){}
-
-  ngOnInit(): void {
-
-  }
-
-
 }
